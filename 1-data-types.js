38377f302d478b167bb54b77a56dd4e884b1a21f
Yani Rodriguez <38408922+Yani010712@users.noreply.github.com>
2019-11-14 00:09:52 -0500

    Day 1 - js solution

---
+++ 1-data-types.js
@@ -0,0 +1,15 @@
+
+    // Declare second integer, double, and String variables.
+    const i2 = parseInt(readLine());
+    const d2 = parseFloat(readLine());
+    const s2 = readLine();
+    // Read and save an integer, double, and String to your variables.
+    // Print the sum of both integer variables on a new line.
+    console.log( i + i2);
+
+    // Print the sum of the double variables on a new line.
+    console.log((d + d2).toFixed(1));
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + s2);
