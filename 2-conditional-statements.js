80ecfbf095cb2940bbd855a834a631c4c2f2b499
Yani Rodriguez <yani010712@gmail.com>
2019-11-17 16:38:37 -0500

    changing name of file

--- 2-conditional-statements.js
+++
@@ -1,9 +0,0 @@
-function main() {
-    const N = parseInt(readLine(), 10);
-
-    if (N % 2 != 0 || N % 2 == 0 && N >= 6 && N <= 20) {
-        console.log('Weird');
-    } else {
-        console.log('Not Weird');
-    }
-}
