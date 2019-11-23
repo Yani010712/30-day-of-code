51c7103626294d28cf1ee31a92d43fefc46b57cf
Yani Rodriguez <yani010712@gmail.com>
2019-11-22 19:50:29 -0500

    Day 18 Queues and Stacks js solution

---
+++ 18-queues-stacks.js
@@ -0,0 +1,10 @@
+function Solution(){
+    this.stack = [];
+    this.queue = [];
+  
+    this.pushCharacter = ch => this.stack.push(ch); 
+    this.enqueueCharacter = ch => this.queue.push(ch);
+    this.popCharacter = () => this.stack.pop();
+    this.dequeueCharacter = () => this.queue.shift();
+  
+  }
